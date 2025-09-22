ec97b1df40a07196d8f29b0a50109f2e95870630
Graphten <graphten06@gmail.com>
2025-09-22 15:20:03 -0600

    added basic functionality

--- input_handler.py
+++ input_handler.py
@@ -4,3 +4,18 @@ License: MIT
 
 Glue code that stores commands that are assigned to inputs. When input is recieved the input is delegated to the associated command.
 """
+from config import *
+
+class InputHandler:
+    def __init__(self):
+        self.keybinds = {}
+
+    # Execute commands for the set of keys passed in
+    def handle_input(self, keys):
+        for key in keys:
+            self.keybinds[key]()
+
+
+    # Bind key to function
+    def bind_key(self, key, function):
+        self.keybinds[key][function]
