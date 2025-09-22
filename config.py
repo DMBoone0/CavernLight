d9ccb3b885f707c1ceef2c63fc483e2ea9933361
Graphten <graphten06@gmail.com>
2025-09-22 13:54:17 -0600

    Added basic imports and window dimensions to config

---
+++ config.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame import Vector2 as vector2
+from pygame import Vector3 as vector3
+from sys import exit
+from os.path import join
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
