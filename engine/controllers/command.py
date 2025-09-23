c4fe99d15affd14a081fdd4a245d320041a434ba
Graphten <graphten06@gmail.com>
2025-09-23 16:42:48 -0600

    initial command implementation

--- engine/controllers/command.py
+++ engine/controllers/command.py
@@ -1,3 +1,16 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any, Optional
+
+class CommandType(Enum):
+    MOVE_FORWARD = auto()
+    MOVE_BACKWARD = auto()
+    TURN_LEFT = auto()
+    TURN_RIGHT = auto()
+    OPEN_MENU = auto()
+    QUIT_GAME = auto()
+
+@dataclass
 class Command:
-    def __init__(self):
-        pass
+    type: CommandType
+    payload: Optional[dict[str, Any]] = None
