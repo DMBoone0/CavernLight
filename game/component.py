682ef681b269ab7e12624a0f32aefe1afc5fc2f8
Graphten <graphten06@gmail.com>
2025-09-23 22:23:50 -0600

    added some initial components

--- game/component.py
+++ game/component.py
@@ -7,3 +7,38 @@ Collection of ECS components.
 
 from dataclasses import dataclass
 
+# Map data
+@dataclass
+class Position:
+    x: int
+    y: int
+
+@dataclass
+class Velocity:
+    dx: int
+    dy: int
+
+@dataclass
+class Renderable:
+    sprite_id: str
+
+
+# Combat Data
+@dataclass
+class Health:
+    max_hp: int
+    curr_hp: int
+
+
+# Categories
+@dataclass
+class Collidable:
+    pass
+
+@dataclass
+class PlayerControlled:
+    pass
+
+@dataclass
+class Encounter:
+    pass
