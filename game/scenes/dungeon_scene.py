ca44fbfb010f545af3185aa1c2029c7f5e467033
Graphten <graphten06@gmail.com>
2025-09-24 00:23:59 -0600

    added move command handling to dungeon

--- game/scenes/dungeon_scene.py
+++ game/scenes/dungeon_scene.py
@@ -9,6 +9,7 @@ from scenes import Scene
 from world import World
 from components import Position, Direction, Collidable, PlayerControlled
 from systems import MovementSystem
+from engine.controllers import Command, CommandType
 
 class DungeonScene(Scene):
     def on_enter(self, prev_scene=None):
@@ -19,16 +20,25 @@ class DungeonScene(Scene):
         # map loading probably.
 
         # spawn player
-        player_id = self.world.create_entity()
-        self.world.add_component(player_id, Position(0,0))
-        self.world.add_component(player_id, Direction(1,0))
-        self.world.add_component(player_id, PlayerControlled())
+        self.player_id = self.world.create_entity()
+        self.world.add_component(self.player_id, Position(0,0))
+        self.world.add_component(self.player_id, Direction(1,0))
+        self.world.add_component(self.player_id, PlayerControlled())
 
     def on_exit(self, next_scene):
         pass
     
     def handle_command(self, command):
-        pass
+        if command.type == CommandType.MOVE_FORWARD:
+            player_direction = self.world.get_component(Direction)[self.player_id]
+            self.movement_system.move(self.player_id, player_direction.x, player_direction.y)
+        elif command.type == CommandType.TURN_RIGHT:
+            self.movement_system.rotate(self.player_id, False)
+        elif command.type == CommandType.TURN_LEFT:
+            self.movement_system.rotate(self.player_id, True)
+        
+        for eid in self.world.get_component(Position):
+            print(Position)
     
     def render(self, renderer):
         pass
