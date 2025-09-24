5c2a14f94b34c3b140c335a04e58f866393fa865
Graphten <graphten06@gmail.com>
2025-09-23 22:16:26 -0600

    basic world implementation

--- game/world.py
+++ game/world.py
@@ -9,4 +9,24 @@ from collections import defaultdict
 from typing import Type
 
 class World:
-    pass
+    def __init__(self):
+        self._next_entity_id = 0
+        # ComponentType -> {entity id: component instance}
+        self.components = defaultdict(dict)
+        self.entities = set()
+
+    def create_entity(self):
+        eid = self._next_entity_id
+        self._next_entity_id += 1
+        self.entities.add(eid)
+    
+    def add_component(self, eid, component):
+        self.components[type(component)][eid] = component
+    
+    def get_component(self, component):
+        return self.components[type(component)].items()
+    
+    def remove_entity(self, eid):
+        self.entities.discard(eid)
+        for component_dict in self.components:
+            component_dict.pop(eid, None)
