5c0f04d2d23d9171eaa83194614e734202894a36
Graphten <graphten06@gmail.com>
2025-09-23 21:47:59 -0600

    added dungeon scene stub

--- game/scene/__init__.py
+++ game/scene/__init__.py
@@ -1,1 +1,3 @@
 # /game/scene
+from .scene import Scene
+from .dungeon_scene import DungeonScene
---
+++ game/scene/dungeon_scene.py
@@ -0,0 +1,11 @@
+"""
+Author: Dillon Boone
+License: MIT
+
+Scene that contains the dungeon world.
+"""
+
+from .scene import Scene
+
+class DungeonScene(Scene):
+    pass
