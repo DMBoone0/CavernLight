5ea05393e25f3171f47647a74606de5236ab58a7
Graphten <graphten06@gmail.com>
2025-09-24 19:40:45 -0600

    map data basic wrapper functionality

--- game/maps/map_data.py
+++ game/maps/map_data.py
@@ -4,3 +4,10 @@ License: MIT
 
 Wrapper class for map data.
 """
+
+class MapData:
+    def __init__(self, tiles, spawns=None, encounters=None, triggers=None) -> None:
+        self.tiles = tiles
+        self.spawns = spawns or {} # { "floor_1": (x,y), "town": (x,y), ... }
+        self.entities = encounters or [] # [ { name: 'slime_1', pos: (x,y), enemies: [SLIME, SLIME, ...], ... }, ... ]
+        self.triggers = triggers or [] # [ { name: 'tuto_1', pos: (x,y), type: TextEvent, ... }, ... ]
